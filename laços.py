cdceaa21473795be34dbe5f2495cfed8f6e93a28
Ratobeso <arthurnunesmarins09@gmail.com>
2025-02-26 17:28:58 -0300

    Add files via upload

---
+++ laços.py
@@ -0,0 +1,12 @@
+for i in range(101):
+    print (f"o numero atual é {i}!")
+
+
+leveis = [1,2,3,4,5,6,7,8,9,10]
+nivel = 0
+
+for level in leveis:
+    nivel = nivel + level
+    print (f"o nivel atual é de {nivel}")
+
+
