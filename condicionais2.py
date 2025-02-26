8687e59e391f77ae1551581ab901d3b95641b4ab
Ratobeso <arthurnunesmarins09@gmail.com>
2025-02-26 17:28:44 -0300

    Add files via upload

---
+++ condicionais2.py
@@ -0,0 +1,10 @@
+rato_opressor = input("o RATO DE ESGOTO joga bem? responda com N para não e S para sim ")
+
+if rato_opressor != "S" :
+    print ("fala direito alienigena")
+elif rato_opressor != "N" :
+    print ("fala direito alienigena")
+elif rato_opressor == "S":
+    print ("ta quase mestre")
+else :
+    print ("ta dou com o gordox")
