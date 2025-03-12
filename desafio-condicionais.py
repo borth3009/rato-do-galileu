74adcfef1d0872d1f661ca3217fd2e595ea018b0
Ratobeso <arthurnunesmarins09@gmail.com>
2025-03-12 16:16:24 -0300

    Add files via upload

---
+++ desafio-condicionais.py
@@ -0,0 +1,8 @@
+email = "arthurreidelas@gmail.com"
+
+email_tentativa = str(input("dijite seu email "))
+
+if email == email_tentativa:
+    print ("acesso sucedido")
+else:
+    print ("acesso negado")
