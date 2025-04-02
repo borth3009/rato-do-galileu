864b2354e8619207a8562546921bf71b2162ea6f
Ratobeso <arthurnunesmarins09@gmail.com>
2025-04-02 17:04:55 -0300

    Add files via upload

---
+++ metodos.py
@@ -0,0 +1,7 @@
+#quero importar a bibilioteca random e utilixar seus metodos ja prontos
+#RANDINT e um metodo que gera um sorteio emtre um intervalo de numeros
+#escolhe 1 item da lista
+Z
+import random
+print (random.randint(1, 100))
+print(random.choice([A , B , C]))
