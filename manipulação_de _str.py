ee5ef48b11fe36fcced57139fcec3ac7ed557ad9
Ratobeso <arthurnunesmarins09@gmail.com>
2025-04-09 16:34:12 -0300

    Add files via upload

--- manipulação_de _str.py
+++ manipulação_de _str.py
@@ -7,3 +7,9 @@ print (plvr[-3])
 pi =3.143341234123412341234
 print (f"numero pi é de {pi}")
 print (f"o numero pi arredondado é de {pi:.2f}")
+
+
+
+megasena = 1234012340913045923412342350135812351203851208350123080
+
+print (f"o premio foi de {megasena:,}")
