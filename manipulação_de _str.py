86255fe43e9f24766f2e5b71225a87fcc15b121d
Ratobeso <arthurnunesmarins09@gmail.com>
2025-04-09 16:30:19 -0300

    Add files via upload

---
+++ manipulação_de _str.py
@@ -0,0 +1,9 @@
+plvr = 'ratobeso'
+import random
+print (plvr[random.randint(0,7)])
+print (plvr[-3])
+
+
+pi =3.143341234123412341234
+print (f"numero pi é de {pi}")
+print (f"o numero pi arredondado é de {pi:.2f}")
