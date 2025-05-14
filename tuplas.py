de9b377c30182eaaad84b1b3517f8011d51eecc7
Ratobeso <arthurnunesmarins09@gmail.com>
2025-05-14 17:25:04 -0300

    Add files via upload

---
+++ tuplas.py
@@ -0,0 +1,13 @@
+#tuplas são imultaveis
+#representadas por ()
+#indexsavel
+#da pra fatiar
+#n tem muitos metodos pois é imultavel
+
+tupla_nick = ("docinhosujo","ratobeso","balãozinho")
+lista_nick = ["docinhosujo","ratobeso","balãozinho"]
+
+lista_nick.append("cashwx")
+print (lista_nick)
+print (tupla_nick.count("docinhosujo"))
+print (tupla_nick.index("ratobeso"))
