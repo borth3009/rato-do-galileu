76761dc8ba139e883a48d0ef2ebe2244cc4d781c
Ratobeso <arthurnunesmarins09@gmail.com>
2025-05-21 17:18:31 -0300

    Add files via upload

---
+++ desafiolistas2.py
@@ -0,0 +1,11 @@
+lista_nomes = [
+    'alvaro',
+    'leticia',
+    'joaquim',
+    'arthur',
+    'ricardo'
+]
+print(lista_nomes[0:4])
+print(lista_nomes[2:5])
+print(lista_nomes[1::2])
+print(lista_nomes[::-1])
