9acf8354bf5094dd8d03f6cc898374a362c93d36
Ratobeso <arthurnunesmarins09@gmail.com>
2025-06-25 16:41:11 -0300

    Add files via upload

--- funções.py
+++ funções.py
@@ -1,11 +1,34 @@
-#criaremos uma função que somara 2 fatores e trara o resultado
-def somar (a,b):
-    soma = a + b
-    return soma
+import random
+#sem parametro e sem retorno
+def boas_vindas ():
+    print ("seja bem vindo!")
+boas_vindas()
 
-pn = int(input("digite o primeiro numero: "))
-sn = int(input("digite o segundo numero: "))
+#sem parametro mas com retorno
+def numero_aleatorio_1433():
+    return random.randint(14,33)
+print (numero_aleatorio_1433())
 
-result = somar(pn , sn)
-print (f"o resultado da soma é {result}")
+#com parametro e com retorno 
+def multiplicação (a,b):
+    return a*b
+print (multiplicação(2,8))
 
+#com parametro mas sem retorno
+def ola_personalisado(nome):
+    print (f'ola {nome} seja bem vindo!')
+ola_personalisado('rato')
+
+#exercicios
+def desconto(valor,porcentagem):
+    return valor*(porcentagem/100)
+print (desconto(200,35))
+
+senha = tuple('rato123')
+senha_verificação = tuple(input('qual a sua senha:'))
+def verificação (senha_verificada):
+    if senha_verificada == senha :
+        return print ('acesso sussedido:')
+    else :
+       return print ('acesso negado:')
+verificação(senha_verificação)
