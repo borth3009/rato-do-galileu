79b3c98432f671c281c3a9ab462b4e9d3a4ad2ae
Ratobeso <arthurnunesmarins09@gmail.com>
2025-07-17 15:01:23 -0300

    Add files via upload

---
+++ notas2.py
@@ -0,0 +1,20 @@
+def calcular_media(lista_notas):
+    if len(lista_notas) == 0:
+        return 0
+    return sum(lista_notas) / len(lista_notas)
+def avaliar_aluno(nome, lista_notas):
+    media = calcular_media(lista_notas)
+
+    if 9 <= media <= 10:
+        print(f"Parabéns, {nome}! Excelente desempenho.")
+    elif 7 <= media <= 8.9:
+        print(f"Muito bem, {nome}! Você está indo bem.")
+    elif 5 <= media <= 6.9:
+        print(f"Atenção, {nome}. Você precisa se dedicar mais.")
+    else:  # Abaixo de 5
+        print(f"Força, {nome}!faz o L kk")
+notas = []
+for n in range(4):
+    notas = float(input(f'digite a nota {n +1}: '))
+
+avaliar_aluno("Rato", notas)
